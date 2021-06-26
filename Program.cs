2e17072ea11f0b51e41bd8cf0269e76dd2b63e6f
MatheusAlmeidaNascimento <86276081+MatheusAlmeidaNascimento@users.noreply.github.com>
2021-06-25 23:00:52 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercicio_Desafio_Repetições
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ultimoElemento = 1;
+            int penultimoElemento = 0;
+
+            var sequenciaDeFibonacci = new List <int> { 1, 1 };
+
+            while (sequenciaDeFibonacci.Count < 20)
+            {
+                var proximoElemento = sequenciaDeFibonacci [penultimoElemento] + sequenciaDeFibonacci [ultimoElemento];
+
+                sequenciaDeFibonacci.Add(proximoElemento);
+
+                ultimoElemento++;
+                penultimoElemento++; 
+            }
+
+            Console.WriteLine("Os vinte primeros números da Sequência de Fibonacci são: ");
+
+            foreach (var elemento in sequenciaDeFibonacci)
+            {
+                Console.WriteLine(elemento);
+            }            
+        }
+    }
+}
